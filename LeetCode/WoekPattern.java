ae906a8cdd74fea22fc124768019862458ceb128
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-16 22:47:06 +0100

    some

---
+++ LeetCode/WoekPattern.java
@@ -0,0 +1,44 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+
+class WordPattern {
+     public static void main(String[] args) {
+        String a = "abba" ,  t = "dog dog dog dog" ; // true
+        System.out.println("########".repeat(3));
+        String b= "aaaa" ,  e = "dog cat cat dog"; // true 
+        String c= "abba" , f = "dog cat cat fish" ; //true 
+        System.out.println(wordPattern(a, t));
+
+        
+     }
+   public static boolean wordPattern(String pattern, String s) {
+    String[] words = s.split(" ");
+    if (pattern.length() != words.length) return false;
+
+    Map<Character, String> map = new HashMap<>();
+    Set<String> usedWords = new HashSet<>();
+
+    for (int i = 0; i < pattern.length(); i++) {
+        char c = pattern.charAt(i);
+        String word = words[i];
+
+        if (map.containsKey(c)) {
+            if (!map.get(c).equals(word)) {
+                return false;
+            }
+        } else {
+            if (usedWords.contains(word)) {
+                return false;
+            }
+            map.put(c, word);
+            usedWords.add(word);
+        }
+    }
+
+    return true;
+}
+
+}
